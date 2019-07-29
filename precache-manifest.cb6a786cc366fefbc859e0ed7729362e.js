a8fd0fed4d186dbf1c8cac52426e6a60b6fc7eeb
NapatKarm <napat.karm@gmail.com>
2019-07-29 14:21:42 -0400

    Updates

---
+++ precache-manifest.cb6a786cc366fefbc859e0ed7729362e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12eba9740b33b1cd5ecd23c67a99122c",
+    "url": "/saarhaber/Pixelate-React/index.html"
+  },
+  {
+    "revision": "220d591c8c4a784cb59e",
+    "url": "/saarhaber/Pixelate-React/static/css/main.cebb4f78.chunk.css"
+  },
+  {
+    "revision": "36124575841ff54c6dea",
+    "url": "/saarhaber/Pixelate-React/static/js/2.86c0466d.chunk.js"
+  },
+  {
+    "revision": "220d591c8c4a784cb59e",
+    "url": "/saarhaber/Pixelate-React/static/js/main.bdd4a3f9.chunk.js"
+  },
+  {
+    "revision": "91c4973251afdfac0a36",
+    "url": "/saarhaber/Pixelate-React/static/js/runtime~main.1353e67a.js"
+  }
+]);
